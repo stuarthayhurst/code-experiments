9c05c79616fe4084492ee7fcaa06ca7de7d9298d
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2024-08-07 01:34:07 +0100

    Add recursive Inkscape cleaner script

---
+++ bash/clean-svgs-recursive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Get a list of directories and the root directory
+readarray -t dirList <<< "$(find ./ -type d)"
+startDir="$(pwd)"
+
+#Loop through directories, generate the full path, and compress and svgs
+for i in "${dirList[@]}"; do
+  svgs=()
+  targetDir="$startDir/${i/"./"}"
+
+  for svg in "$targetDir/"*.svg; do
+    if [[ -f "$svg" ]]; then
+      svgs+=("$svg")
+    fi
+  done
+
+  for svg in "${svgs[@]}"; do
+    if [[ ! -L "$svg" ]]; then
+      echo "$svg"
+      inkscape "--vacuum-defs" "--export-filename=$svg" "$svg"
+    fi
+  done
+done
