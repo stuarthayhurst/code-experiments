c023c638f7ca60125e52d773b30c891fd5a33d44
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2024-08-12 15:33:33 +0100

    Add wget and git to chroot

--- minecraft-server/build-chroot.sh
+++ minecraft-server/build-chroot.sh
@@ -7,5 +7,5 @@ sudo debootstrap unstable "$ENVPATH"
 sudo mkdir -p "${ENVPATH}/${DIR}"
 
 sudo mount --bind /proc "${ENVPATH}/proc"
-sudo chroot "$ENVPATH" bash -c "apt update; echo 'y' | apt install default-jre-headless; exit"
+sudo chroot "$ENVPATH" bash -c "apt update; echo 'y' | apt install default-jre-headless wget git; exit"
 sudo umount "${ENVPATH}/proc"
