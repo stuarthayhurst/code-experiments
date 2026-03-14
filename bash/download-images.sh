598660bb3196f1809188119ad14bb802a121fe3b
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-03-14 19:32:13 +0000

    Check the checksums and signatures of downloaded images when supported

--- bash/download-images.sh
+++ bash/download-images.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Pass the version to download as the first argument
+#Pass "firmware" as the second argumen to download images with non-free firmware
+#Use SIGNATUREKEYFILE="" to set a file to use for verifying checksum signatures
+#  - Only required if not downloading from a Debian machine with 'debian-keyring' installed, as the local copy will be used by default
+
 debianVersion="$1"
 
 if [[ "$1" == "" ]]; then
@@ -40,11 +45,74 @@ else
   exit 1
 fi
 
-for url in "${imageUrls[@]}"; do
+if [[ -x "$(command -v sha512sum)" ]]; then
+  checkImageChecksum="true"
+
+  if [[ -x "$(command -v gpg)" ]]; then
+    checkChecksumSignature="true"
+
+    keyFileName="$SIGNATUREKEYFILE"
+    if [[ "$SIGNATUREKEYFILE" == "" ]]; then
+      keyFileName="/usr/share/keyrings/debian-role-keys.gpg"
+    fi
+
+  fi
+fi
+
+#Download a file from a URL and optionally supply a file name
+downloadFile() {
+  url="$1"
+  fileName="$2"
+  if [[ "$2" == "" ]]; then
+    fileName="${url##*/}"
+  fi
+
   if [[ "$downloadCommand" == "curl" ]]; then
-    filename="${url##*/}"
-    $downloadCommand -L -o "$filename" "$url"
-  else
-    $downloadCommand "$url"
+    $downloadCommand -L -o "$fileName" "$url"
+  elif [[ "$downloadCommand" == "wget" ]]; then
+    $downloadCommand -O "$fileName" "$url"
+  fi
+}
+
+echo "Using '$downloadCommand' to download images"
+if [[ "$checkImageChecksum" == "true" ]]; then
+  echo "Downloaded images will have their checksums verified"
+  if [[ "$checkChecksumSignature" == "true" ]]; then
+    echo "Checksums will have their signatures verified with '$keyFileName'"
+  fi
+fi
+echo -e "\n"
+
+for url in "${imageUrls[@]}"; do
+  downloadFile "$url"
+
+  #Verify image checksum
+  if [[ "$checkImageChecksum" == "true" ]]; then
+    tempPath="$(mktemp -d)"
+
+    checksumUrl="${url%/*}/SHA512SUMS"
+    checksumFileName="$tempPath/debian-image-downloader-checksum"
+    downloadFile "$checksumUrl" "$checksumFileName"
+
+    if ! sha512sum --ignore-missing -c "$checksumFileName"; then
+      echo "Checksum verification failed"
+      rm -rf "$tempPath"
+      exit 1
+    fi
+
+    #Verify checksum signature
+    if [[ "$checkChecksumSignature" == "true" ]]; then
+      signatureUrl="${url%/*}/SHA512SUMS.sign"
+      signatureFileName="$tempPath/debian-image-downloader-signature"
+      downloadFile "$signatureUrl" "$signatureFileName"
+
+      if ! gpg --keyring "$keyFileName" --verify "$signatureFileName" "$checksumFileName"; then
+        echo "Checksum signature verification failed"
+        rm -rf "$tempPath"
+        exit 1
+      fi
+    fi
+
+    rm -rf "$tempPath"
   fi
 done
