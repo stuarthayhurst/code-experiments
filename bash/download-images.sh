e5acd91f66c9f399ec35983a8789221181af2ed7
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-03-14 18:25:49 +0000

    Add some validation to the arguments

--- bash/download-images.sh
+++ bash/download-images.sh
@@ -2,12 +2,23 @@
 
 debianVersion="$1"
 
+if [[ "$1" == "" ]]; then
+  echo "Expected a version, got nothing"
+  exit 1
+elif [[ "$1" == *"firmware" ]]; then
+  echo "Expected firmware toggle as the second argument"
+  exit 1
+fi
+
 if [[ "$2" == *"firmware" ]]; then
   outputPath="$(pwd)/Firmware"
   imageUrls=("https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion+nonfree/amd64/iso-cd/firmware-$debianVersion-amd64-netinst.iso" #Netinst firmware
   "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion+nonfree/amd64/iso-dvd/firmware-$debianVersion-amd64-DVD-1.iso" #DVD firmware
   "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion-live+nonfree/amd64/iso-hybrid/debian-live-$debianVersion-amd64-gnome+nonfree.iso" #GNOME live firmware
   "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion-live+nonfree/amd64/iso-hybrid/debian-live-$debianVersion-amd64-standard+nonfree.iso") #Standard live firmwa>
+elif [[ "$2" != "" ]]; then
+  echo "Expected firmware toggle as the second argument, got '$2'"
+  exit 1
 else
   outputPath="$(pwd)"
   imageUrls=("https://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/debian-$debianVersion-amd64-netinst.iso" #Netinst
