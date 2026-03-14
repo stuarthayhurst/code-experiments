be0cecacd7ff404d895869a00a1164a7ce5b926a
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-03-14 18:22:12 +0000

    Update URL for firmware images

--- bash/download-images.sh
+++ bash/download-images.sh
@@ -4,10 +4,10 @@ debianVersion="$1"
 
 if [[ "$2" == *"firmware" ]]; then
   outputPath="$(pwd)/Firmware"
-  imageUrls=("https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current/amd64/iso-cd/firmware-$debianVersion-amd64-netinst.iso" #Netinst firmware
-  "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current/amd64/iso-dvd/firmware-$debianVersion-amd64-DVD-1.iso" #DVD firmware
-  "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-$debianVersion-amd64-gnome+nonfree.iso" #GNOME live firmware
-  "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-$debianVersion-amd64-standard+nonfree.iso") #Standard live firmware
+  imageUrls=("https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion+nonfree/amd64/iso-cd/firmware-$debianVersion-amd64-netinst.iso" #Netinst firmware
+  "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion+nonfree/amd64/iso-dvd/firmware-$debianVersion-amd64-DVD-1.iso" #DVD firmware
+  "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion-live+nonfree/amd64/iso-hybrid/debian-live-$debianVersion-amd64-gnome+nonfree.iso" #GNOME live firmware
+  "https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/archive/$debianVersion-live+nonfree/amd64/iso-hybrid/debian-live-$debianVersion-amd64-standard+nonfree.iso") #Standard live firmwa>
 else
   outputPath="$(pwd)"
   imageUrls=("https://cdimage.debian.org/cdimage/release/current/amd64/iso-cd/debian-$debianVersion-amd64-netinst.iso" #Netinst
