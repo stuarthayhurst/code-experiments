41c589c5f3b03436de57529883e16683ca9fd267
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-05-28 15:03:27 +0100

    Unmount /proc from the build environment before cleaning

--- debian-kernel/clean.sh
+++ debian-kernel/clean.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/bash
 
+if [[ -d "build/proc" ]]; then
+  sudo umount -q build/proc
+fi
+
+if [[ -d "build-mainline/proc" ]]; then
+  sudo umount -q build-mainline
+fi
+
 sudo rm -rvf build/ build-mainline/
