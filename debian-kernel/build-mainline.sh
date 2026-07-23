9196ec75a3b492ef2073e0f441e14bffb6b26cd5
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-07-23 19:44:09 +0100

    Remove extra space in /proc announcement

--- debian-kernel/build-mainline.sh
+++ debian-kernel/build-mainline.sh
@@ -13,6 +13,6 @@ sudo chroot build-mainline ./internal-mainline.sh "$1"
 
 echo -e "\nSetup complete, swap into chroot, then setup '.config' and build with 'make bindeb-pkg -j$(nproc)'"
 
-echo -e "\n /proc has been bind mounted to 'build-mainline/proc'"
+echo -e "\n/proc has been bind mounted to 'build-mainline/proc'"
 echo " - When done, unmount with 'sudo umount build-mainline/proc'"
 echo " - If required, remount with 'sudo mount --bind /proc build-mainline/proc'"
