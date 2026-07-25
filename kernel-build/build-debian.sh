1d11821dd025b333afb1ccde23e20599e1ec64f7
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2026-07-25 23:28:05 +0100

    Accept custom build environment path for Debian builds

--- kernel-build/build-debian.sh
+++ kernel-build/build-debian.sh
@@ -2,9 +2,27 @@
 #shellcheck disable=SC2181
 
 #Set up a build environment and source code for a Debian kernel
-#Pass "experimental" to use the kernel version from experimental
+#Optionally pass "experimental" to use the kernel version from experimental
+#Optionally pass a name for a new directory to use as the build environment
 #Any patches in "patches/" will be applied before the build starts
 #Requires debootstrap and an internet connection
+
+#Process passed arguments
+internalArgs=""
+buildDir="build-debian"
+for arg in "${@}"; do
+  #Pass "experimental" to chroot script if passed
+  if [[ "$arg" == "experimental" ]]; then
+    internalArgs="experimental"
+  fi
+
+  #Use the first non-empty, non-"experimental" argument as the build environment name
+  if [[ "$buildDir" == "build-debian" ]]; then
+    if [[ "$arg" != "" ]] && [[ "$arg" != "experimental" ]]; then
+      buildDir="$arg"
+    fi
+  fi
+done
 
 #Check for root
 if [[ "$(id -u)" != "0" ]]; then
@@ -13,8 +31,8 @@ if [[ "$(id -u)" != "0" ]]; then
 fi
 
 #Bail if an existing instance is present
-if [[ -d "build-debian" ]]; then
-  echo "build-debian/ already exists, exiting" >&2
+if [[ -d "$buildDir" ]]; then
+  echo "'$buildDir' already exists, exiting" >&2
   exit 1
 fi
 
@@ -24,22 +42,22 @@ if [[ "$1" == "experimental" ]]; then
 fi
 
 #Install the base system
-debootstrap --include=ca-certificates unstable build-debian
+debootstrap --include=ca-certificates unstable "$buildDir"
 if [[ "$?" != "0" ]]; then
   echo "Failed to install the base system, exiting" >&2
   exit 1
 fi
 
 #Copy the patches and set up script
-mkdir build-debian/patches
-cp patches/* build-debian/patches/
-cp internal-debian.sh build-debian/
+mkdir "$buildDir/patches"
+cp patches/* "$buildDir/patches/"
+cp internal-debian.sh "$buildDir"
 
 #Bind mount /proc and run the internal build script
-mount --bind /proc build-debian/proc
-chroot build-debian ./internal-debian.sh "$1"
+mount --bind /proc "$buildDir/proc"
+chroot "$buildDir" ./internal-debian.sh "$internalArgs"
 if [[ "$?" != "0" ]]; then
   echo "Failed to build the kernel, exiting" >&2
   exit 1
 fi
-umount build-debian/proc
+umount "$buildDir/proc"
